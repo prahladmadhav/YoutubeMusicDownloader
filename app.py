83a1651b01d49c60a6b6cda06383f59662b90b57
prahladmadhav <prahladmadhav@gmail.com>
2020-03-06 23:33:58 +0530

    (0002) Youtube Search

--- app.py
+++ app.py
@@ -1,13 +1,24 @@
 from selenium import webdriver
+from bs4 import BeautifulSoup
+import os 
 import time
 
+#Enter Name of Song
+songName = input("Enter Song Name:\t")
 
 #Open Driver
 driver = webdriver.Firefox()
 
-#
+#Open 
 Youtube = "https://www.youtube.com"
 driver.get(Youtube)
+
+#Search Song
+element = driver.find_element_by_name("search_query")
+element.send_keys(songName)
+driver.find_element_by_id("search-icon-legacy").click()
+
+
 
 time.sleep(10)
 driver.close()
